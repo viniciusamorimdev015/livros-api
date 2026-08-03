f6b884b93e4ac1faf7e9a83b031ed1bed7080ddb
viniciusamorimdev015 <viniciusamorimdev015@gmail.com>
2026-08-03 19:03:38 -0300

    aplicando o livroRepository

--- src/livros/livros.service.ts
+++ src/livros/livros.service.ts
@@ -1,71 +1,59 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateLivroDto } from './dto/create-livro.dto';
 import { UpdateLivroDto } from './dto/update-livro.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LivroEntity } from './entities/livro.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class LivrosService {
 
-    private livros = [
-        {
-            id: 1,
-            titulo: 'livro 1',
-            autor: 'algume',
-            genero: 'Terror',
-            status: 'Lido'
-        }
-    ]
+    constructor(
+        @InjectRepository(LivroEntity) 
+        private readonly livroRepository: Repository<LivroEntity>
+    ) {}
 
-    create(createLivroDto: CreateLivroDto){
-        const livro = {
-            id: this.livros.length + 1,
-            titulo: createLivroDto.titulo,
-            autor: createLivroDto.autor,
-            genero: createLivroDto.genero,
-            status: createLivroDto.status
-        }
-
-        this.livros.push(livro)
-        return livro
+    async create(createLivroDto: CreateLivroDto){ 
+        const livro = this.livroRepository.create(createLivroDto)
+        return await this.livroRepository.save(livro)
     }
 
-    findAll(){
-        return this.livros
+    async findAll(){
+        return await this.livroRepository.find()
     }
 
-    findOne(id: string){
-        const livro = this.livros.find(l => l.id === +id)
+    async findOne(id: string){
+        const livro = await this.livroRepository.findOneBy({ id: +id })
+
+        if(!livro) {
+            throw new BadRequestException(`Livro de ID ${id} não encontrado`)
+        }
+
+        return livro 
+    }
+
+
+    async update(id: string, updateLivroDto: UpdateLivroDto){
+      const livro = await this.livroRepository.preload({
+        id: +id,
+        ...updateLivroDto
+      })
+
+      if(!livro){
+        throw new BadRequestException(`Livro de ID ${id} não encontrado`)
+      }
+
+      return await this.livroRepository.save(livro)
+    }
+
+    async remove(id: string){
+        const livro = await this.findOne(id)
 
         if(!livro){
             throw new BadRequestException(`Livro de ID ${id} não encontrado`)
         }
 
-        return livro
-    }
-
-
-    update(id: string, updateLivroDto: UpdateLivroDto){
-       const livro = this.livros.find(l => l.id == +id)
-
-        if(!livro){
-            throw new BadRequestException(`Livro de ID ${id} não encontrado`)
-        }
-
-        if(updateLivroDto.titulo)  livro.titulo = updateLivroDto.titulo
-        if(updateLivroDto.autor)  livro.autor = updateLivroDto.autor
-        if(updateLivroDto.genero)  livro.genero = updateLivroDto.genero
-        if(updateLivroDto.status)  livro.status = updateLivroDto.status
-
-        return livro
-    }
-
-    remove(id: string){
-        const indexLivro = this.livros.findIndex(l => l.id === +id)
-
-        if(indexLivro === -1){
-            throw new BadRequestException(`Livro com ID ${id} não encontrado`)
-        }
-
-        this.livros.splice(indexLivro, 1)
+        await this.livroRepository.remove(livro)    
 
         return { message: `Livro de ID ${id} removido com sucesso` }
     }
